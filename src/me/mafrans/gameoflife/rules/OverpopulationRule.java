13102562f1c8467a64f273b61bbc05b77a22ffa6
Mafrans <malte.kluft@gmail.com>
2020-10-05 10:40:20 +0200

    Create rules

---
+++ src/me/mafrans/gameoflife/rules/OverpopulationRule.java
@@ -0,0 +1,10 @@
+package me.mafrans.gameoflife.rules;
+
+import me.mafrans.gameoflife.Cell;
+
+public class OverpopulationRule implements Rule {
+    @Override
+    public void apply(Cell cell) {
+
+    }
+}
---
+++ src/me/mafrans/gameoflife/rules/UnderpopulationRule.java
@@ -0,0 +1,10 @@
+package me.mafrans.gameoflife.rules;
+
+import me.mafrans.gameoflife.Cell;
+
+public class UnderpopulationRule implements Rule {
+    @Override
+    public void apply(Cell cell) {
+
+    }
+}
