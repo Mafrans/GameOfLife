ad12ada5c210151105d3269c2a3df2c54972c75a
Mafrans <malte.kluft@gmail.com>
2020-10-05 10:58:04 +0200

    Set neighbors to be points

--- src/me/mafrans/gameoflife/Cell.java
+++ src/me/mafrans/gameoflife/Cell.java
@@ -1,17 +1,28 @@
 package me.mafrans.gameoflife;
 
+import java.awt.Point;
 import java.util.Set;
 
 public class Cell {
     public int x;
     public int y;
     public boolean isAlive;
-    public Set<Cell> neighbors;
+    public Point[] neighbors;
     public Grid grid;
 
     public Cell(int x, int y) {
         this.x = x;
         this.y = y;
+        this.neighbors = new Point[]{
+                new Point(x + 1, y),
+                new Point(x + 1, y + 1),
+                new Point(x, y + 1),
+                new Point(x - 1, y + 1),
+                new Point(x - 1, y),
+                new Point(x - 1, y - 1),
+                new Point(x, y - 1),
+                new Point(x + 1, y - 1)
+        };
     }
 
     public Cell(int x, int y, boolean isAlive) {
