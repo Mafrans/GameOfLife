47ca2d19dc63504299a6e4c4e95e7e31b5d98ee6
marcus.billman <54668482+marcusbillman@users.noreply.github.com>
2020-10-06 12:45:37 +0200

    Add logic for ReproductionRule

--- src/me/mafrans/gameoflife/rules/ReproductionRule.java
+++ src/me/mafrans/gameoflife/rules/ReproductionRule.java
@@ -3,6 +3,8 @@ package me.mafrans.gameoflife.rules;
 import me.mafrans.gameoflife.Cell;
 import me.mafrans.gameoflife.EvolutionManager;
 
+import java.awt.*;
+
 public class ReproductionRule extends Rule {
     public ReproductionRule(EvolutionManager evolutionManager) {
         super(evolutionManager);
@@ -10,6 +12,17 @@ public class ReproductionRule extends Rule {
 
     @Override
     public void apply(Cell cell) {
+        if (cell.isAlive) return; //Only apply rule on dead cells
 
+        int count = 0;
+        for(Point p : cell.neighbors) {
+            Cell c = cell.grid.getCell(p.x, p.y);
+
+            if(c.isAlive) count++;
+        }
+
+        if(count == 3) {
+            evolutionManager.nextGrid.setCell(cell.x, cell.y, true);
+        }
     }
 }
