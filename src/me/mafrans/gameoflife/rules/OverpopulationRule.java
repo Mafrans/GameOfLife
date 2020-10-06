9757d279e40490ad108a4ca8ed342e4e3beef704
Mafrans <malte.kluft@gmail.com>
2020-10-06 12:31:51 +0200

    Create OverpopulationRule

--- src/me/mafrans/gameoflife/rules/OverpopulationRule.java
+++ src/me/mafrans/gameoflife/rules/OverpopulationRule.java
@@ -3,6 +3,8 @@ package me.mafrans.gameoflife.rules;
 import me.mafrans.gameoflife.Cell;
 import me.mafrans.gameoflife.EvolutionManager;
 
+import java.awt.*;
+
 public class OverpopulationRule extends Rule {
     public OverpopulationRule(EvolutionManager evolutionManager) {
         super(evolutionManager);
@@ -10,5 +12,15 @@ public class OverpopulationRule extends Rule {
 
     @Override
     public void apply(Cell cell) {
+        int count = 0;
+        for(Point p : cell.neighbors) {
+            Cell c = cell.grid.getCell(p.x, p.y);
+
+            if(c.isAlive) count++;
+        }
+
+        if(count > 3) {
+            evolutionManager.nextGrid.setCell(cell.x, cell.y, false);
+        }
     }
 }
