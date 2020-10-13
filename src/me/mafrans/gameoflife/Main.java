17f770d477c4c38786cb8b9405cfbaa4486ec6b9
marcus.billman <54668482+marcusbillman@users.noreply.github.com>
2020-10-13 13:11:44 +0200

    Separate Window class from Main

--- src/me/mafrans/gameoflife/Main.java
+++ src/me/mafrans/gameoflife/Main.java
@@ -1,45 +1,7 @@
 package me.mafrans.gameoflife;
 
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
-
 public class Main {
     public static void main(String[] args) {
-        int rows = 300;
-        int columns = 300;
-        int scale = 4;
-
-        GameOfLife gameOfLife = new GameOfLife(columns, rows);
-        Renderer renderer = new Renderer(gameOfLife, scale);
-        JFrame frame = new JFrame();
-
-        frame.setTitle("Malte och Marcus Spel om Livet");
-        frame.add(renderer);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setResizable(false);
-        frame.setVisible(true);
-        Insets insets = frame.getInsets();
-        frame.setSize(
-                columns * scale + insets.left + insets.right,
-                rows * scale + insets.top + insets.bottom
-        );
-        frame.setLocationRelativeTo(null);
-
-        frame.addKeyListener(new KeyListener() {
-            @Override
-            public void keyPressed(KeyEvent e) {
-                gameOfLife.evolutionManager.step();
-                renderer.draw();
-                Benchmarker.next("Render Time");
-            }
-
-            @Override public void keyReleased(KeyEvent e) { }
-            @Override public void keyTyped(KeyEvent e) { }
-        });
-
-        gameOfLife.grid.populateRandomly();
-        renderer.draw();
+        new Window(100, 100, 8, "Malte och Marcus Spel om Livet");
     }
 }
